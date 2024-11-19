a5d03295ea38d184a691a901ea1153bd4967729b
saarts <saartssamo@gmail.com>
2024-11-19 15:56:12 +0200

    Update ex04.py

--- ex04.py
+++ ex04.py
@@ -1,7 +1,9 @@
 """
 EX 4
 
-Luua kontuuride leidmise programm, mis loetleb õige koguse objekte pildil
-ning kuvab selle numbri ekraanil.
+Tehke ise video asjadest, mida soovite kokku loendada.
+
+Luua kontuuride leidmise programm, mis loetleb õige koguse objekte videos
+ning kuvab selle numbri ekraanil. Lisaks, joonistab kastid ümber objektide.
 
 """
